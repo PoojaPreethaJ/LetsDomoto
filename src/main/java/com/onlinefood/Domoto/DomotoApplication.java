7bb6d38f649c31bfb90a6736b0977030bc2cd6f9
PoojaPreethaJ <poojajothiappan23@gmail.com>
2022-03-18 00:50:02 +0530

    first commit

---
+++ src/main/java/com/onlinefood/Domoto/DomotoApplication.java
@@ -0,0 +1,13 @@
+package com.onlinefood.Domoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomotoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onlinefood/Domoto/DomotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlinefood.Domoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
